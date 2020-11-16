6805e995ee9f4ac988f0fa8ff67f83400e738634
Themba100 <bsc-com-09-18@cc.ac.mw>
2020-11-16 18:58:22 +0200

    setting to null form fields and clearing local storage

--- utils.js
+++ utils.js
@@ -9,3 +9,20 @@ const validation = (...fields) => {
         return true;
     }
 }
+const nullify = (fields) => {
+    if (fields.length !== 0) {
+        fields.map(field => {
+            document.getElementById(field).value = '';
+        });
+    }
+}
+const clearAll = () => {
+    // localStorage
+    var storage = window.localStorage.getItem('users');
+    // check if it exists, then assign users object to an empty array
+    // otherwise do nothing
+    if (storage !== undefined) {
+        window.localStorage.setItem('users', []);
+        alert("Successfully clear storage!!")
+    }
+}
